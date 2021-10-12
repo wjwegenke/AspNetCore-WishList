a821a061ed14135efa9bb1213f8abb6eaa19d038
Warren Wegenke <wwegenke@omnitech-inc.com>
2021-10-12 17:01:23 -0500

    Created the ItemController.

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            Item itemToDelete = _context.Items.FirstOrDefault(i => i.Id == Id);
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
